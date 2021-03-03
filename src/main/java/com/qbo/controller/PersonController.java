13a93dd363b497e3f9c2abd6bf488cec5ebbd57c
Luis Angel Salvatierra Aquino <luisangel.salvat@gmail.com>
2021-03-02 20:49:53 -0500

    actualización.

--- src/main/java/com/qbo/controller/PersonController.java
+++ src/main/java/com/qbo/controller/PersonController.java
@@ -1,12 +1,17 @@
 package com.qbo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qbo.exception.ResourceNotFoundException;
 import com.qbo.model.Person;
 import com.qbo.service.PersonService;
 
@@ -18,8 +23,23 @@ public class PersonController {
 	PersonService personService;
 	
 	@GetMapping("/persons")
-	public List<Person> getAllPersons(){
-		return personService.getAllPersons();
+	public ResponseEntity<List<Person>> getAllPersons(){
+		List<Person> persons = new ArrayList<>();
+		personService.getAllPersons().forEach(persons::add);
+		if(persons.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<>(personService.getAllPersons(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/person/{id}")
+	public ResponseEntity<Person> getPersonById(@PathVariable("id") Integer id) 
+			throws ResourceNotFoundException {
+		Person person = personService
+				.findById(id)
+				.orElseThrow(() -> 
+				new ResourceNotFoundException("Not found Person whit id = "+id));
+		return new ResponseEntity<>(person, HttpStatus.OK);
 	}
 	
 	
