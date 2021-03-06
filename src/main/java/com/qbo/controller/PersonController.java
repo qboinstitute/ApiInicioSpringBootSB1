f630fa106985a258907606603c4cd0f174f9614c
Luis Angel Salvatierra Aquino <luisangel.salvat@gmail.com>
2021-03-06 09:50:43 -0500

    actualización final

--- src/main/java/com/qbo/controller/PersonController.java
+++ src/main/java/com/qbo/controller/PersonController.java
@@ -6,8 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,6 +45,39 @@ public class PersonController {
 				new ResourceNotFoundException("Not found Person whit id = "+id));
 		return new ResponseEntity<>(person, HttpStatus.OK);
 	}
+	
+	@PostMapping("/person")
+	public ResponseEntity<Person> createPerson(@RequestBody Person person){
+		Person _person = personService.save(person);
+		return new ResponseEntity<>(_person, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/person/{id}")
+	public ResponseEntity<Person> updatePerson(@PathVariable("id")Integer id, 
+			@RequestBody Person person) throws ResourceNotFoundException {
+		Person oldPerson = personService.findById(id)
+				.orElseThrow(()-> 
+				new ResourceNotFoundException("Not found Person with ID = "+ id));
+		return new ResponseEntity<>(personService.update(oldPerson, person), 
+				HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/person/{id}")
+	public ResponseEntity<Person> deletePerson(@PathVariable("id")Integer id)
+			throws ResourceNotFoundException{
+		Person person = personService.findById(id)
+				.orElseThrow(()->
+				new ResourceNotFoundException("Not found Person with ID= "+ id));
+		return new ResponseEntity<>(personService.delete(person), 
+				HttpStatus.NO_CONTENT);
+	}
+	
+	@DeleteMapping("/person")
+	public ResponseEntity<HttpStatus> deleteAllPerson(){
+		personService.deleteAll();
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+	
 	
 	
 }
